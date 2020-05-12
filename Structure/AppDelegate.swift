013c9450f49f47b618c62a60ec927955d45378d5
Ian McDonald <ianjoemcdonald@gmail.com>
2020-05-12 18:22:46 -0300

    AppDelegate

--- Structure/AppDelegate.swift
+++ Structure/AppDelegate.swift
@@ -11,10 +11,13 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var globalUsers: Users!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        globalUsers = Users()
+        
         return true
     }
 
